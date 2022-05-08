f5841f363d2746b494248628ceaa8183cbd2aa2b
Alex Galo <alexgalo246@gmail.com>
2022-05-07 20:54:24 -0600

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import { Login } from './Components/Login';
+// import { Login } from './Components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RegistryForm } from './Components/RegistryForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login> </Login>
+    {/* <Login> </Login> */}
+    <RegistryForm> </RegistryForm>
   </React.StrictMode>
 );
 
