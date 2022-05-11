4f2c46c40ee49714c3bd37710e00c0e31c5dc907
Alex Galo <alexgalo246@gmail.com>
2022-05-10 23:13:24 -0600

    adding routes to components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Login } from './Components/Login';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { RegistryForm } from './Components/RegistryForm';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <Login> </Login> */}
-    <RegistryForm> </RegistryForm>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 
