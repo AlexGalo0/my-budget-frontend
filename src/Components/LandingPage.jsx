89d0ec7d8a3334e28bffde25f5852eb5aa46a891
Alex Galo <alexgalo246@gmail.com>
2022-05-10 23:30:06 -0600

    minor changes

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const LandingPage = () => {
+  return (
+    <>
+    <h1 id='titulo'>Bienvenido a MyBudget!</h1>
+    
+    
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 // import { Login } from './Components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { RegistryForm } from './Components/RegistryForm';
+import { LandingPage } from './Components/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Login> </Login> */}
-    <RegistryForm> </RegistryForm>
+    {/* <RegistryForm> </RegistryForm> */}
+    <LandingPage> </LandingPage>
   </React.StrictMode>
 );
 
