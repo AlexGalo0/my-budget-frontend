f167bbdfe9e604a50eb4a91c6d9db110baf3ec69
Alex Galo <alexgalo246@gmail.com>
2022-05-11 00:49:23 -0600

    minor change

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { RegistryForm } from './Components/RegistryForm';
-import { LandingPage } from './Components/LandingPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
