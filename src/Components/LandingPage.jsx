c9b88dda5a6b2543628dd5ed3803d004ae07f79a
Alex Galo <alexgalo246@gmail.com>
2022-05-11 00:23:25 -0600

    setting style in landingPage

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import "../Assets/LandingPage.css";
 export const LandingPage = () => {
   return (
     <>
-    <h1 id='titulo'>Bienvenido a MyBudget!</h1>
-    
-    
+      <h1 id="titulo">Bienvenido a MyBudget!</h1>
+      <section id="options">
+        <button class="btn btn1">Registrate</button>
+        <button class="btn btn2">Inicia Sesion</button>
+      </section>
     </>
-  )
-}
+  );
+};
