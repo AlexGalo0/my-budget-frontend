6480a96862d3cc0d6601bf234ec9750f225a1d3f
Alex Galo <alexgalo246@gmail.com>
2022-05-12 20:18:10 -0600

    adding routing to landing buttons

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../Assets/LandingPage.css";
 export const LandingPage = () => {
   return (
-    <main id='bg-landing'>
+    <main id="bg-landing">
       <h1 id="titulo">Bienvenido a MyBudget!</h1>
       <section id="options">
-        <button class="btn btn1">Registrate</button>
+        <Link to="/registry">
+          <button class="btn btn1">Registrate</button>
+        </Link>
+        <Link to='login'>
         <button class="btn btn2">Inicia Sesion</button>
+        
+        </Link>
       </section>
     </main>
   );
