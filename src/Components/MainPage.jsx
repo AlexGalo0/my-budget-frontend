0432bf1457986ae808b2da5d63cba9fbea9af7ef
Alex Galo <alexgalo246@gmail.com>
2022-05-17 00:08:32 -0600

    adding buttons in mainpage

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import '../Assets/Main.css'
+import React from "react";
+import "../Assets/Main.css";
 export const MainPage = () => {
   return (
-    <main id='bg-main'>
-      <div id='navbar'>
-        <h1 id='welcome-text'>Bienvenido a tu cuenta, Alex! </h1>
+    <main id="bg-main">
+      <div id="navbar">
+        <h1 id="welcome-text">Bienvenido a tu cuenta, Alex! </h1>
       </div>
 
-
+      <div id="categories">
+        <button>View Expenses <br>
+        </br>and Incomes!</button>
+        <button>Transfer Money!</button>
+        <button>General Dashboard</button>
+      </div>
     </main>
-  )
-}
+  );
+};
+
