dc5228565d1f40267e233278c2e8163829822bd9
Alex Galo <alexgalo246@gmail.com>
2022-05-16 23:59:52 -0600

    styling navbar in main

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import '../Assets/Main.css'
 export const MainPage = () => {
   return (
-    <h1>MainPage</h1>
+    <main id='bg-main'>
+      <div id='navbar'>
+        <h1 id='welcome-text'>Bienvenido a tu cuenta, Alex! </h1>
+      </div>
+
+
+    </main>
   )
 }
