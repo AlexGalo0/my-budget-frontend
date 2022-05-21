d165bdb96c4e42429ea5345e7514d25c8a90a495
Alex Galo <alexgalo246@gmail.com>
2022-05-21 10:29:45 -0600

    adding style in transfer page

--- src/Components/GeneralDash.jsx
+++ src/Components/GeneralDash.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const GeneralDash = () => {
   return (
-    <h1>GeneralDash</h1>
-  )
+    <></>
+    )
 }
--- src/Components/Transfer.jsx
+++ src/Components/Transfer.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
-
+import React from "react";
+import "../Assets/Transfer.css";
 export const Transfer = () => {
   return (
-    <h1>Transfer</h1>
-  )
-}
+    <main id="bg-transfer">
+      <h1 id="titulo">Realiza una Transferencia, Alex Galo </h1>
+
+      <form id="formulario">
+
+      <label>
+        Ingrese la cuenta de la que desea retirar
+      </label>
+      <select name="" id="">
+        <option value="">Cuenta de Ahorro Carro</option>
+      </select>
+
+
+        <label htmlFor="name" className="inputs">
+          Ingrese el Número de Cuenta al que desea Depositar
+        </label>
+        <input
+          type="text"
+          name="name"
+          // value={formValues.firstName}
+          // onChange={handleChange}
+          placeholder="26472231"
+        />
+
+
+        <label htmlFor="email" className="inputs">
+          Ingrese el Monto (LPS):{" "}
+        </label>
+        <input
+          type="text"
+          name="email"
+          // value={formValues.email}
+          // onChange={handleChange}
+          placeholder="500"
+        />
+
+        <button type="submit" id="boton" className="button-58">
+          Enviar
+        </button>
+      </form>
+    </main>
+  );
+};
